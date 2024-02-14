d6ad75a3beb0af6c174f56cfed5b18b30d94619b
Mahmoud Hamdy <mahmouashwah@gmail.com>
2024-02-14 23:58:11 +0200

    app name

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:travel_app/features/home/ui/views/home_view.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const TravelApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class TravelApp extends StatelessWidget {
+  const TravelApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Travel App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
