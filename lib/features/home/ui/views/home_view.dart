539e429d982fd1b0ed65526d35799c5f16d3a07f
Mahmoud Hamdy <mahmouashwah@gmail.com>
2024-02-15 13:42:20 +0200

    app bar and text widget

--- lib/features/home/ui/views/home_view.dart
+++ lib/features/home/ui/views/home_view.dart
@@ -1,12 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app/core/widgets/app_large_text.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home'),
-    );
+    return Scaffold(
+        body: Padding(
+      padding: const EdgeInsets.only(top: 70.0, left: 20, right: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Icon(
+                Icons.menu,
+                size: 30,
+                color: Colors.black54,
+              ),
+              Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.grey.withOpacity(0.5),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          AppLargeText(text: "Discover"),
+          const SizedBox(
+            height: 30,
+          ),
+        ],
+      ),
+    ));
   }
 }
