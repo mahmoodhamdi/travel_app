dc1b705b4aaffeb3ab9e650a46303977449faf35
Mahmoud Hamdy <mahmouashwah@gmail.com>
2024-02-15 15:10:14 +0200

    finish home view "UI"

---
+++ lib/features/home/data/model/travel_model.dart
@@ -0,0 +1,5 @@
+class TravelModel{
+  final String name;
+  final String image;
+  TravelModel({required this.name, required this.image});
+}
--- lib/features/home/ui/views/home_view.dart
+++ lib/features/home/ui/views/home_view.dart
@@ -3,6 +3,7 @@ import 'package:travel_app/core/misc/colors.dart';
 import 'package:travel_app/core/widgets/app_large_text.dart';
 import 'package:travel_app/core/widgets/app_text.dart';
 import 'package:travel_app/features/home/data/model/place_model.dart';
+import 'package:travel_app/features/home/data/model/travel_model.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -32,6 +33,24 @@ class _HomeViewState extends State<HomeView> with TickerProviderStateMixin {
         location: "Canada,Banff",
       ),
     ];
+    List<TravelModel> travels = [
+      TravelModel(
+        name: "Kayaking",
+        image: "assets/images/kayaking.png",
+      ),
+      TravelModel(
+        name: "Hiking",
+        image: "assets/images/hiking.png",
+      ),
+      TravelModel(
+        name: "Snorkeling",
+        image: "assets/images/snorkling.png",
+      ),
+      TravelModel(
+        name: "Ballooning",
+        image: "assets/images/balloning.png",
+      )
+    ];
     return Scaffold(
         body: Padding(
       padding: const EdgeInsets.only(top: 70.0, left: 20, right: 20),
@@ -57,11 +76,11 @@ class _HomeViewState extends State<HomeView> with TickerProviderStateMixin {
             ],
           ),
           const SizedBox(
-            height: 40,
+            height: 30,
           ),
           AppLargeText(text: "Discover"),
           const SizedBox(
-            height: 30,
+            height: 20,
           ),
           SizedBox(
             child: TabBar(
@@ -144,6 +163,56 @@ class _HomeViewState extends State<HomeView> with TickerProviderStateMixin {
                 child: const Text("Emotions"),
               ),
             ]),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            AppLargeText(
+              text: "Explore more",
+              size: 22,
+            ),
+            const SizedBox(
+              width: 20,
+            ),
+            AppText(
+              text: "See all",
+              color: AppColors.textColor1,
+            )
+          ]),
+          const SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+            height: 110,
+            width: double.maxFinite,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: travels.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                      margin: const EdgeInsets.only(right: 20),
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: 80,
+                              height: 80,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                image: DecorationImage(
+                                  image: AssetImage(travels[index].image),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            AppText(
+                              text: travels[index].name,
+                              color: AppColors.textColor2,
+                            )
+                          ]));
+                }),
           )
         ],
       ),
