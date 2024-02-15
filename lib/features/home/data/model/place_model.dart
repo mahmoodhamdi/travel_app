657a39d6079522c3f0eb1a7747ec9fb693085295
Mahmoud Hamdy <mahmouashwah@gmail.com>
2024-02-15 14:42:26 +0200

    finish places tab view

---
+++ lib/features/home/data/model/place_model.dart
@@ -0,0 +1,6 @@
+class PlaceModel {
+  final String name;
+  final String image;
+  final String location;
+  PlaceModel({required this.name, required this.image, required this.location});
+}
--- lib/features/home/ui/views/home_view.dart
+++ lib/features/home/ui/views/home_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:travel_app/core/misc/colors.dart';
 import 'package:travel_app/core/widgets/app_large_text.dart';
+import 'package:travel_app/core/widgets/app_text.dart';
+import 'package:travel_app/features/home/data/model/place_model.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -13,6 +15,23 @@ class _HomeViewState extends State<HomeView> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     TabController tabController = TabController(length: 3, vsync: this);
+    List<PlaceModel> places = [
+      PlaceModel(
+        name: "Cascades",
+        image: "assets/images/mountain.jpeg",
+        location: "Canada,Banff",
+      ),
+      PlaceModel(
+        name: "Sydney",
+        image: "assets/images/us.webp",
+        location: "US,Sydney",
+      ),
+      PlaceModel(
+        name: "Cascades",
+        image: "assets/images/mountain.jpeg",
+        location: "Canada,Banff",
+      ),
+    ];
     return Scaffold(
         body: Padding(
       padding: const EdgeInsets.only(top: 70.0, left: 20, right: 20),
@@ -54,6 +73,7 @@ class _HomeViewState extends State<HomeView> with TickerProviderStateMixin {
                 color: AppColors.mainColor,
                 radius: 4,
               ),
+              dividerHeight: 0,
               controller: tabController,
               tabs: const [
                 Tab(
@@ -72,9 +92,51 @@ class _HomeViewState extends State<HomeView> with TickerProviderStateMixin {
             height: 300,
             width: double.maxFinite,
             child: TabBarView(controller: tabController, children: [
-              Container(
-                child: const Text("Places"),
-              ),
+              ListView.builder(
+                  itemCount: places.length,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: const EdgeInsets.only(right: 15, top: 10),
+                      width: 200,
+                      height: 300,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          image: DecorationImage(
+                            image: AssetImage(places[index].image),
+                            fit: BoxFit.cover,
+                          )),
+                      child: Padding(
+                        padding: const EdgeInsets.only(bottom: 20.0, left: 20),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            AppText(
+                              text: places[index].name,
+                              color: Colors.white,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                const Icon(
+                                  Icons.location_on,
+                                  color: Colors.white,
+                                ),
+                                const SizedBox(
+                                  width: 5,
+                                ),
+                                AppText(
+                                  text: places[index].location,
+                                  color: Colors.white,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  }),
               Container(
                 child: const Text("Inspirations"),
               ),
