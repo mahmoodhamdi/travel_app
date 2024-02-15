221341e014af7e1c083dab2cb17eb8ef785ac788
Mahmoud Hamdy <mahmouashwah@gmail.com>
2024-02-15 14:19:45 +0200

    custom CircleTabIndicator

--- lib/features/home/ui/views/home_view.dart
+++ lib/features/home/ui/views/home_view.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app/core/misc/colors.dart';
 import 'package:travel_app/core/widgets/app_large_text.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
   @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> with TickerProviderStateMixin {
+  @override
   Widget build(BuildContext context) {
+    TabController tabController = TabController(length: 3, vsync: this);
     return Scaffold(
         body: Padding(
       padding: const EdgeInsets.only(top: 70.0, left: 20, right: 20),
@@ -37,8 +44,73 @@ class HomeView extends StatelessWidget {
           const SizedBox(
             height: 30,
           ),
+          SizedBox(
+            child: TabBar(
+              labelColor: Colors.black,
+              labelPadding: const EdgeInsets.only(left: 20, right: 20),
+              unselectedLabelColor: Colors.grey,
+              indicatorSize: TabBarIndicatorSize.label,
+              indicator: const CircleTabIndicator(
+                color: AppColors.mainColor,
+                radius: 4,
+              ),
+              controller: tabController,
+              tabs: const [
+                Tab(
+                  text: "Places",
+                ),
+                Tab(
+                  text: "Inspirations",
+                ),
+                Tab(
+                  text: "Emotions",
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 300,
+            width: double.maxFinite,
+            child: TabBarView(controller: tabController, children: [
+              Container(
+                child: const Text("Places"),
+              ),
+              Container(
+                child: const Text("Inspirations"),
+              ),
+              Container(
+                child: const Text("Emotions"),
+              ),
+            ]),
+          )
         ],
       ),
     ));
+  }
+}
+
+class CircleTabIndicator extends Decoration {
+  final Color color;
+  final double radius;
+  const CircleTabIndicator({required this.color, required this.radius});
+  @override
+  BoxPainter createBoxPainter([VoidCallback? onChanged]) {
+    return _CirclePainter(color: color, radius: radius);
+  }
+}
+
+class _CirclePainter extends BoxPainter {
+  final Color color;
+  final double radius;
+  _CirclePainter({required this.color, required this.radius});
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
+    Paint paint = Paint();
+    paint.color = color;
+    paint.isAntiAlias = true;
+    Offset circleOffset = Offset(
+        offset.dx + configuration.size!.width / 2 - radius / 2,
+        configuration.size!.height - radius);
+    canvas.drawCircle(circleOffset, radius, paint);
   }
 }
